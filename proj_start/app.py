c57dcfa5333cef0397d5f73bfcde5c7b30cc378d
Aidan N <bobersnip808@gmail.com>
2021-11-14 02:59:25 -0500

    Added proj with api call functionality
    This project has api call functionality. Check the readme for how to use it. You need to have MySQL installed.

---
+++ proj_start/app.py
@@ -0,0 +1,203 @@
+######################################
+# author ben lawson <balawson@bu.edu>
+# Edited by: Craig Einstein <einstein@bu.edu>
+######################################
+# Some code adapted from
+# CodeHandBook at http://codehandbook.org/python-web-application-development-using-flask-and-mysql/
+# and MaxCountryMan at https://github.com/maxcountryman/flask-login/
+# and Flask Offical Tutorial at  http://flask.pocoo.org/docs/0.10/patterns/fileuploads/
+# see links for further understanding
+###################################################
+
+import flask
+from flask import Flask, Response, request, render_template, redirect, url_for
+from flaskext.mysql import MySQL
+import flask_login
+import requests
+import json
+
+# for image uploading
+import os
+import base64
+
+mysql = MySQL()
+app = Flask(__name__)
+app.secret_key = 'ayyylmao'  # Change this!
+
+# These will need to be changed according to your creditionals
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'HOtenxm810191'
+app.config['MYSQL_DATABASE_DB'] = 'photoshare'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+
+# begin code used for login
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+
+conn = mysql.connect()
+cursor = conn.cursor()
+cursor.execute("SELECT email from Users")
+users = cursor.fetchall()
+
+
+def getUserList():
+    cursor = conn.cursor()
+    cursor.execute("SELECT email from Users")
+    return cursor.fetchall()
+
+
+class User(flask_login.UserMixin):
+    pass
+
+
+@login_manager.user_loader
+def user_loader(email):
+    users = getUserList()
+    if not(email) or email not in str(users):
+        return
+    user = User()
+    user.id = email
+    return user
+
+
+@login_manager.request_loader
+def request_loader(request):
+    users = getUserList()
+    email = request.form.get('email')
+    if not(email) or email not in str(users):
+        return
+    user = User()
+    user.id = email
+    cursor = mysql.connect().cursor()
+    cursor.execute(
+        "SELECT password FROM Users WHERE email = '{0}'".format(email))
+    data = cursor.fetchall()
+    pwd = str(data[0][0])
+    user.is_authenticated = request.form['password'] == pwd
+    return user
+
+
+'''
+A new page looks like this:
+@app.route('new_page_name')
+def new_page_function():
+	return new_page_html
+'''
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if flask.request.method == 'GET':
+        return '''
+			   <form action='login' method='POST'>
+				<input type='text' name='email' id='email' placeholder='email'></input>
+				<input type='password' name='password' id='password' placeholder='password'></input>
+				<input type='submit' name='submit'></input>
+			   </form></br>
+		   <a href='/'>Home</a>
+			   '''
+    # The request method is POST (page is recieving data)
+    email = flask.request.form['email']
+    cursor = conn.cursor()
+    # check if email is registered
+    if cursor.execute("SELECT password FROM Users WHERE email = '{0}'".format(email)):
+        data = cursor.fetchall()
+        pwd = str(data[0][0])
+        if flask.request.form['password'] == pwd:
+            user = User()
+            user.id = email
+            flask_login.login_user(user)  # okay login in user
+            # protected is a function defined in this file
+            return flask.redirect(flask.url_for('protected'))
+
+    # information did not match
+    return "<a href='/login'>Try again</a>\
+			</br><a href='/register'>or make an account</a>"
+
+
+@app.route('/logout')
+def logout():
+    flask_login.logout_user()
+    return render_template('hello.html', message='Logged out', top_users=getTopScoreUsers())
+
+
+@login_manager.unauthorized_handler
+def unauthorized_handler():
+    return render_template('unauth.html')
+
+# you can specify specific methods (GET/POST) in function header instead of inside the functions as seen earlier
+
+
+@app.route("/api_req", methods=['GET'])
+def api_req():
+    return render_template('api_req.html')
+
+
+@app.route("/api_req", methods=['POST'])
+def make_req():
+    try:
+        URL = "https://api.edamam.com/api/recipes/v2?type=public&"
+        api_key_append = "&app_id=4c5b6d9d&app_key=09c2de772eaeb7fd5d30b135fb041c8f"
+        ingredients = request.form.get('ingredients')
+        ingredients = "q=" + ingredients
+        query_url = URL + ingredients + api_key_append
+        print(query_url)
+        response = requests.get(query_url)
+        # print(response.text["hits"])
+
+        recipe_name = []
+        recipe_image = []
+        recipe_ingredients = []
+        index = [x for x in range(20)]
+
+        api_data = json.loads(response.text)["hits"][0:20]
+        for hit in api_data:
+            recipe_name += [hit["recipe"]["label"]]
+            # print(recipe_name)
+            recipe_image += [hit["recipe"]["image"]]
+            # print(recipe_image)
+            recipe_ingredients += [hit["recipe"]["ingredientLines"]]
+            # print(recipe_ingredients)
+
+        return render_template('req_display.html', recipe_name=recipe_name, recipe_image=recipe_image, recipe_ingredients=recipe_ingredients, index=index)
+
+        # Do something here :XXXXXXXXXXXXXXXXXXXXXXXXx
+
+    except:
+        # this prints to shell, end users will not see this (all print statements go to shell)
+        print("couldn't find all tokens")
+        return flask.redirect(flask.url_for('api_req'))
+
+
+@ app.route("/req_display", methods=['GET'])
+def req_display():
+    return render_template('req_diplay.html')
+
+    # cursor = conn.cursor()
+    # test = isEmailUnique(email)
+    # if test:
+    # 	print(cursor.execute("INSERT INTO Users (first_name, last_name, email, birth_date, hometown, gender, password, score)"
+    # 						 "VALUES ('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}', '{7}')".format(first_name, last_name, email, birth_date, hometown, gender, password, 0)))
+
+    # 	# Remember to edit this ^^^^ print statement for the insert!!!!!!!!!!!
+
+    # 	conn.commit()
+    # 	#log user in
+    # 	user = User()
+    # 	user.id = email
+    # 	flask_login.login_user(user)
+    # 	return render_template('hello.html', name=email, message='Account Created!', top_users=getTopScoreUsers())
+    # else:
+
+
+# default page
+@ app.route("/", methods=['GET'])
+def hello():
+    return render_template('hello.html', message='Welecome to Photoshare')
+
+
+if __name__ == "__main__":
+    # this is invoked when in the shell  you run
+    # $ python app.py
+    app.run(port=5000, debug=True)
